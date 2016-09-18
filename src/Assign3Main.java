f824fdbff93a4871df38b4d8b717958200e9dbda
WSiaB <johnl@kirkersoft.com>
2016-09-18 15:07:01 -0700

    Replaced tabs with spaces for consistency.

--- src/Assign3Main.java
+++ src/Assign3Main.java
@@ -1,124 +1,118 @@
 
 public class Assign3Main {
 
-	public static void main(String[] args) {
-		//testing some cards
-		
-		Card sample1 = new Card();
-		Card sample2 = new Card('2', Card.Suit.SPADES);
-		Card sample3 = new Card('7', Card.Suit.CLUBS);
-		Card sample4 = new Card('Q', Card.Suit.DIAMONDS);
-		
-		//testing toString()
-		System.out.println(sample1.toString());
-		System.out.println(sample2.toString());
-		System.out.println(sample3.toString());
-		System.out.println(sample4.toString());
-		
-		//testing set()
-		sample1.set('5', Card.Suit.HEARTS);
-		System.out.println("New sample1: " + sample1.toString());
-	}
-	
+   public static void main(String[] args) {
+      //testing some cards
+
+      Card sample1 = new Card();
+      Card sample2 = new Card('2', Card.Suit.SPADES);
+      Card sample3 = new Card('7', Card.Suit.CLUBS);
+      Card sample4 = new Card('Q', Card.Suit.DIAMONDS);
+
+      //testing toString()
+      System.out.println(sample1.toString());
+      System.out.println(sample2.toString());
+      System.out.println(sample3.toString());
+      System.out.println(sample4.toString());
+
+      //testing set()
+      sample1.set('5', Card.Suit.HEARTS);
+      System.out.println("New sample1: " + sample1.toString());
+   }
 }
 
 class Card {
-	//create the enum for suit
-	public enum Suit {
-		SPADES, DIAMONDS, HEARTS, CLUBS
-	}
-	
-	//create values for value and suit
-	private char value;
-	private Suit suit;
-	private boolean errorFlag;
-	
-	//default constructor
-	public Card() {
-		this('A', Suit.SPADES);
-	}
-	
-	//constructor to set the card's value and suit
-	public Card(char value, Suit suit) {
-		set(value, suit);
-	}
-	
-	public boolean set(char value, Suit suit) {
-		//calling method to check validity
-		boolean validityCheck = isValid(value, suit);
+   //create the enum for suit
+   public enum Suit {
+      SPADES, DIAMONDS, HEARTS, CLUBS
+   }
+
+   //create values for value and suit
+   private char value;
+   private Suit suit;
+   private boolean errorFlag;
+
+   //default constructor
+   public Card() {
+      this('A', Suit.SPADES);
+   }
+
+   //constructor to set the card's value and suit
+   public Card(char value, Suit suit) {
+      set(value, suit);
+   }
+
+   public boolean set(char value, Suit suit) {
+      //calling method to check validity
+      boolean validityCheck = isValid(value, suit);
+
+      //if the values are valid, set the value and suit
+      if (validityCheck == true) {
+         this.value = Character.toUpperCase(value);
+         this.suit = suit;
+
+         //set error flag to false
+         this.errorFlag = false;
+      }
+      else {
+         //if the validity check comes back false, set errorflag to true
+         this.errorFlag = true;
+      }
+
+      return validityCheck;
+   }
+
+   private boolean isValid(char value, Suit suit) {
+      //making a character array to check validity
+      char[] vals = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
 		
-		//if the values are valid, set the value and suit
-		if (validityCheck == true) {
-			this.value = Character.toUpperCase(value);
-			this.suit = suit;
-			
-			//set error flag to false
-			this.errorFlag = false;
-		}
-		
-		else {
-			//if the validity check comes back false, set errorflag to true
-			this.errorFlag = true;
-		}
-		
-		return validityCheck;
-	}
-	
-	private boolean isValid(char value, Suit suit) {
-		//making a character array to check validity
-		char[] vals = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
-				
-		//initializing boolean
-		boolean check = false;
-				
-		//checking the value against the character array vals
-		for (char c : vals) {
-			if (Character.toUpperCase(value) == c) {
-				check = true;
-			}
-		}
-				
-		return check;		
-	}
-	
-	public String toString() {
-		//intialize string
-		String display = "";
-		
-		//if the errorflag is true, set display as "invalid"
-		if (this.getErrorFlag() == true) {
-			display = "Invalid card.";
-		}
-		else {
-			display = this.value + " of " + this.suit;
-		}
-		
-		return display;
-	}
-	
-	//creating the accessors
-	public char getValue() {
-		return value;
-	}
-	
-	public Suit getSuit() {
-		return suit;
-	}
-	
-	public boolean getErrorFlag() {
-		return errorFlag;
-	}
-	
-	//checking the current instance of value and suit against the values of the passed in card
-	public boolean equals(Card card) {
-		boolean checkCard = false;
-		
-		if (this.getSuit() == card.getSuit()) {
-			if (this.getValue() == card.getValue()) {
-				checkCard = true;
-			}
-		}
-		
-		return checkCard;
-	}
+      //initializing boolean
+      boolean check = false;
+
+      //checking the value against the character array vals
+      for (char c : vals) {
+         if (Character.toUpperCase(value) == c) {
+            check = true;
+         }
+      }
+
+      return check;		
+   }
+
+   public String toString() {
+      //initialize string
+      String display = "";
+      //if the errorflag is true, set display as "invalid"
+      if (this.getErrorFlag() == true) {
+         display = "Invalid card.";
+      }
+      else {
+         display = this.value + " of " + this.suit;
+      }
+      return display;
+   }
+
+   //creating the accessors
+   public char getValue() {
+      return value;
+   }
+
+   public Suit getSuit() {
+      return suit;
+   }
+
+   public boolean getErrorFlag() {
+      return errorFlag;
+   }
+
+   //checking the current instance of value and suit against the values of the passed in card
+   public boolean equals(Card card) {
+      boolean checkCard = false;
+      if (this.getSuit() == card.getSuit()) {
+         if (this.getValue() == card.getValue()) {
+            checkCard = true;
+         }
+      }
+      return checkCard;
+   }
 }
