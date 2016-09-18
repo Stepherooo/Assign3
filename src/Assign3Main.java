e4df1760c81290fdcc8b6c36ee0a3427d7d401a8
Stephie <sgan@csumb.edu>
2016-09-18 12:29:19 -0700

    Initial commit
    Started work on the card class.

---
+++ src/Assign3Main.java
@@ -0,0 +1,21 @@
+
+public class Assign3Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
+
+class Card {
+	//create the enum for suit
+	public enum Suit {
+		SPADES, DIAMONDS, HEARTS, CLUBS
+	}
+	
+	//create values for value and suit
+	private char value;
+	private Suit suit;
+	private boolean errorFlag;
+}
